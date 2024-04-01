13db80d773412dd18d903fd7d9826338360e03c0
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-04-01 21:18:53 +0200

    Update Ratelimit

--- middleware/ratelimit.js
+++ middleware/ratelimit.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit')
 
 const rateLimitMiddleware = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 60 * 1000,
+  max: 20,
   message: "You have exceeded the rate limit. Please try again in 15 minutes.",
   headers: true,
 });
