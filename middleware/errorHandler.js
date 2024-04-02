3a777a24a403b432cb97bff0e4e02c003655a04d
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-04-02 21:53:54 +0200

    Commit

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,18 +1,24 @@
-function errorHandler(err, req, res, next) {
+const User = require('../server/models/User');
+
+async function errorHandler(err, req, res, next) {
   if (err.code === 11000) {
-    res.status(500).json({ error: "Username already exists" });
+    const user = await User.find()
+    res.render('register', {errorMessage: "Username already exists.", user})
   }
   if (err.name === "ValidationError") {
-    res.status(500).json({ error: "Minimum password length is 4 characters."})
+    const user = await User.find()
+    res.render('register', {errorMessage: "Minimum password length is 4 characters.", user})
   }
-  if (err.message === "incorrect password") {
-    res.status(500).json({ error : "Incorrect password."})
+  if (err.message === "Incorrect password.") {
+    const user = await User.find()
+    res.render('login', {errorMessage: err.message, user})
   }
-  if (err.message === "incorrect username") {
-    res.status(500).json({ error : "User not found."})
+  if (err.message === "User not found.") {
+    const user = await User.find()
+    res.render('login', {errorMessage: err.message, user})
   }
   console.error(err.stack);
-  res.status(500).json({ error: "Internal Server Error" });
+  res.status(500).json({ error: "Internal Server Error."})
 }
 
 module.exports = errorHandler;
